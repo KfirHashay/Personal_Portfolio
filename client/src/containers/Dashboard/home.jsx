c1a4c8a55beb3319540160453711fdf41147797c
Kfir Hashay <kfirhashay@outlook.com>
2023-06-09 11:44:58 +0300

    FireBase Test wih func

--- client/src/containers/Dashboard/home.jsx
+++ client/src/containers/Dashboard/home.jsx
@@ -1,23 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import instance from '../../http-common';
 
-//const http = 'http://localhost:8080/api';
+//const http = 'https://us-central1-kfir-portfolio.cloudfunctions.net/app/entries';
 
 function Home() {
-  const [backendData, setBackendData] = useState(null);
+  const [backendData, setBackendData] = useState([]);
 
   useEffect(() => {
-    instance.get('/').then((response) => {
+    instance.get('/entries').then((response) => {
       setBackendData(response.data);
     });
   }, []);
+
+  console.log(backendData);
 
   return (
     <div className="DashboardHome">
       <p>This is A Dashboard</p>
 
-      <div className="data">Test</div>
+      <div className="data">
+        {backendData.map((demo, index) => (
+          <div>{demo.text}</div>
+        ))}
+      </div>
     </div>
   );
 }
